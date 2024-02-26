4ee30c6115c5cdc26d9bb3732531bf1051260d8d
Dobber Peas <dobberpe@student.21-school.ru>
2024-02-26 17:46:18 +0300

    completed my funcs, added some possibly usable and nan inf definitions

--- src/s21_math.c
+++ src/s21_math.c
@@ -1,9 +1,61 @@
 #include "s21_math.h"
 
-long double pow(double base, double exp) {
-    long double result = 1.0;
-
-    while (exp) {
-
+pair s21_factorial(int n) {
+    pair result;
+    if (n == 0) {
+        result.first = 1;
+        result.second = 1;
+    } else {
+        pair tmp = s21_factorial(n - 1);
+        result.first = tmp.second;
+        result.second = tmp.first + tmp.second;
     }
+
+    return result;
+}
+
+long double s21_pow(double base, double exp) {
+    long double result = 1;
+
+    if (exp) {
+        if (!s21_fmod(exp, 1)) {
+            while (exp) {
+                if (s21_fmod(exp, 2)) {
+                    result = exp < 0 ? result / base : result * base;
+                    --exp;
+                } else {
+                    base *= base;
+                    exp /= 2;
+                }
+            }
+        } else {
+            result = s21_exp(exp * s21_log(base));
+        }
+    }
+
+    return result;
+}
+
+long double s21_sqrt(double x) {
+    long double n, ni = 1;
+    
+    do {
+        n = ni;
+        ni = (n + (x / n)) / 2;
+    } while (s21_abs(n - ni) >= S21_EPS);
+
+    return n;
+}
+
+long double s21_fmod(double x, double y) {
+    return (y == 0 || s21_isnan(y) || s21_isnan(x)) ? S21_NANL : s21_isinf(y) ? x : x - (long double)s21_floor(x / y) * y;
+}
+
+bool s21_isnan(double n) {
+    d_bits d = {n};
+    return d.bits == 18444492273895866368;
+}
+
+bool s21_isinf(double n) {
+    return n == S21_INF;
 }
--- src/s21_math.h
+++ src/s21_math.h
@@ -1,21 +1,47 @@
 #ifndef S21_MATH_H
 #define S21_MATH_H
 
-int abs(int x);
-long double ceil(double x);
-long double floor(double x);
-long double fabs(double x);
-long double sqrt(double x);
-long double pow(double base, double exp);
-long double exp(double x);
-long double log(double x);
-long double fmod(double x, double y);
+#include <stdbool.h>
 
-long double sin(double x);
-long double cos(double x);
-long double tan(double x);
-long double asin(double x);
-long double acos(double x);
-long double atan(double x);
+#define S21_EPS 1E-7
+#define S21_NANL 0.0l/0.0l
+#define S21_INFL 1.0l/0.0l
+#define S21_NAN 0.0/0.0
+#define S21_INF 1.0/0.0
+
+typedef struct {
+    double first;
+    double second;
+} pair;
+
+typedef union {
+    long double full;
+    unsigned short bits[5];
+} ld_bits;
+
+typedef union {
+    double full;
+    unsigned long long bits;
+} d_bits;
+
+int s21_abs(int x);
+long double s21_ceil(double x);
+long double s21_floor(double x);
+long double s21_fabs(double x);
+long double s21_sqrt(double x);
+long double s21_pow(double base, double exp);
+long double s21_exp(double x);
+long double s21_log(double x);
+long double s21_fmod(double x, double y);
+
+bool s21_isnan(double n);
+bool s21_isinf(double n);
+
+long double s21_sin(double x);
+long double s21_cos(double x);
+long double s21_tan(double x);
+long double s21_asin(double x);
+long double s21_acos(double x);
+long double s21_atan(double x);
 
 #endif
