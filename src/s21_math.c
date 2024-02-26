3b5f47181746a10b48965e6871b1a9dac29e23ed
Dobber Peas <dobberpe@student.21-school.ru>
2024-02-26 16:00:26 +0300

    start. added ascii pics for makefile))

---
+++ src/s21_math.c
@@ -0,0 +1,9 @@
+#include "s21_math.h"
+
+long double pow(double base, double exp) {
+    long double result = 1.0;
+
+    while (exp) {
+
+    }
+}
---
+++ src/s21_math.h
@@ -0,0 +1,21 @@
+#ifndef S21_MATH_H
+#define S21_MATH_H
+
+int abs(int x);
+long double ceil(double x);
+long double floor(double x);
+long double fabs(double x);
+long double sqrt(double x);
+long double pow(double base, double exp);
+long double exp(double x);
+long double log(double x);
+long double fmod(double x, double y);
+
+long double sin(double x);
+long double cos(double x);
+long double tan(double x);
+long double asin(double x);
+long double acos(double x);
+long double atan(double x);
+
+#endif
