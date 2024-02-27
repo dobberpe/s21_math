287075f4c368a5fb31591edaead8a940f9639912
Maria Khlebnikova <norma-83@mail.ru>
2024-02-27 12:58:54 +0300

    modified in s21_math.c - s21_log

--- src/s21_math.c
+++ src/s21_math.c
@@ -22,15 +22,15 @@ long double s21_exp(double x) {
     result = 1 / result;
   }
   if (result > S21_double_MAX) {
-    result = S21_INF;
+    result = S21_INFL;
   }
   return result;
 }
 
 long double s21_fabs(double x) {
   long double result;
-  if (x != x || x == S21_INF || x == -S21_INF) {
-    result = (x != x) ? S21_NAN : S21_INF;
+  if (x != x || x == S21_INFL || x == -S21_INFL) {
+    result = (x != x) ? S21_NANL : S21_INFL;
   } else {
     result = (x < 0) ? -x : x;
   }
@@ -50,25 +50,30 @@ long double s21_floor(double x) {
 }
 
 long double s21_log(double x) {
+  long double result;
+
   if (x <= 0.0) {
-    return -S21_INFL;
-  }
-  if (x == 1.0) {
-    return 0.0;
-  }
-  if (x < 1.0) {
+    result = -S21_INFL;
+  } else if (x == 1.0) {
+    result = 0.0;
+  } else if (x < 1.0) {
     x = 1 / x;
-    return -s21_log(x);
+    result = -s21_log(x);
+  } else {
+    result = 0.0;
+    long double term = (x - 1.0) / (x + 1.0);
+    long double term_squared = term * term;
+    long double current_term = term;
+    long double power = 1.0;
+
+    for (int i = 0; i < 100; i++) {
+      result += current_term / power;
+      current_term *= term_squared;
+      power += 2.0;
+    }
+
+    result = 2 * result;
   }
-  long double result = 0.0;
-  long double term = (x - 1.0) / (x + 1.0);
-  long double term_squared = term * term;
-  long double current_term = term;
-  long double power = 1.0;
-  for (int i = 0; i < 100; i++) {
-    result += current_term / power;
-    current_term *= term_squared;
-    power += 2.0;
-  }
-  return 2 * result;
+
+  return result;
 }
