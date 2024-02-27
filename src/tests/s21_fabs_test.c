bb336b35cdba1b2dcd96aacdae3e358abd9ad57a
Dobber Peas <dobberpe@student.21-school.ru>
2024-02-27 21:52:15 +0300

    added edge case tests to floor. atan timeout in tests. log and floor don't pass

--- src/tests/s21_fabs_test.c
+++ src/tests/s21_fabs_test.c
@@ -15,11 +15,43 @@ START_TEST(fabs_fn) {
 }
 END_TEST
 
+START_TEST(s21_fabs_inf) {
+  // INF
+  ck_assert_ldouble_eq(fabs(S21_INF), s21_fabs(S21_INF));
+}
+END_TEST
+
+START_TEST(s21_fabs_inf_negative) {
+  // INF
+  ck_assert_ldouble_eq(fabs(-S21_INF), s21_fabs(-S21_INF));
+}
+END_TEST
+
+START_TEST(s21_fabs_nan) {
+  // NAN
+  ld_bits check = {fabs(S21_NAN)};
+  ld_bits result = {s21_fabs(S21_NAN)};
+  for (int i = 0; i < 5; ++i)  ck_assert_uint_eq(check.bits[i], result.bits[i]);
+}
+END_TEST
+
+START_TEST(s21_fabs_nan_negative) {
+  // NAN
+  ld_bits check = {fabs(-S21_NAN)};
+  ld_bits result = {s21_fabs(-S21_NAN)};
+  for (int i = 0; i < 5; ++i)  ck_assert_uint_eq(check.bits[i], result.bits[i]);
+}
+END_TEST
+
 Suite *s21_fabs_cases(void) {
   Suite *c = suite_create("s21_fabs_cases");
   TCase *tc = tcase_create("s21_fabs_tc");
 
   tcase_add_test(tc, fabs_fn);
+  tcase_add_test(tc, s21_fabs_inf);
+  tcase_add_test(tc, s21_fabs_inf_negative);
+  tcase_add_test(tc, s21_fabs_nan);
+  tcase_add_test(tc, s21_fabs_nan_negative);
 
   suite_add_tcase(c, tc);
 
