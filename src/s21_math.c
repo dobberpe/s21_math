6e841d57d645c928ffeba578730f80064eda11a8
Maria Khlebnikova <norma-83@mail.ru>
2024-02-29 11:41:10 +0300

    modified s21_floor (it passes all the tests); half-fixed s21_log(tests s21_log_inf_negative and s21_log_negative are don't work)

--- src/s21_math.c
+++ src/s21_math.c
@@ -1,5 +1,15 @@
 #include "s21_math.h"
 
+bool s21_isnan(double n) {
+  d_bits d = {n};
+  return d.bits == NAN_BITS;
+}
+
+bool s21_isinf(double n) {
+  d_bits d = {n};
+  return d.bits == INF_BITS;
+}
+
 long double s21_exp(double x) {
   long double result = 1;
   long double term = 1;
@@ -34,57 +44,94 @@ long double s21_fabs(double x) {
   }
   return result;
 }
+long double s21_pow(double base, double exp) {
+  long double result = 1;
 
-
-long double s21_floor(double x) {
-  long double result = x;
-
-  if (s21_isnan(x) || s21_isinf(x)) {
-    return x;
-  }
-  long long int int_part = (long long int)x;
-  if (result < 0 && 1.0/result == 0.0) {
-    return -S21_INF;
-  }
-  if (x < 0.0 && result != int_part) {
-    result = int_part - 1.0;
-  } else {
-    result = int_part;
+  if (!exp) result = 1;
+  else if (s21_isnan(base) || s21_isnan(-base)) result = !s21_fmod(exp, 2) ? s21_fabs(base) : base;
+  else if (base == 1 || (base == -1 && !s21_fmod(exp, 2)) || (base == -1 && s21_isinf(s21_fabs(exp)))) result = 1;
+  else if ((s21_isinf(s21_fabs(base)) && exp > 1) || (s21_isinf(exp) && base && s21_fabs(base) != 1)) result = S21_INFL;
+  else if ((s21_isinf(s21_fabs(base)) && exp < 0) || (s21_isinf(-exp) && base && s21_fabs(base) != 1)) result = 0;
+  else if (exp) {
+    if (!s21_fmod(exp, 1)) {
+      while (exp) {
+        if (s21_fmod(exp, 2)) {
+          result = exp < 0 ? result / base : result * base;
+          exp += exp < 0 ? 1 : -1;
+        } else {
+          base *= base;
+          exp /= 2;
+        }
+      }
+    } else {
+      result = s21_exp(exp * s21_log(base));
+    }
   }
 
   return result;
 }
 
+long double s21_fmod(double x, double y) {
+  return s21_isnan(x) || s21_isnan(-x) ? x : s21_isnan(y) ? S21_NANL : s21_isnan(-y) || x == S21_INF || -x == S21_INF || y == 0 ? -S21_NANL : s21_isinf(y) || s21_isinf(-y) ? x : x - (long double)s21_floor(x / y) * y;
+}
+
+// long double s21_floor(double x) {
+//   long double result = x;
+//
+//   if (s21_isnan(x) || s21_isinf(x)) {
+//     return x;
+//   }
+//   long long int int_part = (long long int)x;
+//   if (result < 0 && 1.0/result == 0.0) {
+//     return -S21_INF;
+//   }
+//   if (x < 0.0 && result != int_part) {
+//     result = int_part - 1.0;
+//   } else {
+//     result = int_part;
+//   }
+//
+//   return result;
+// }
+
+long double s21_floor(double x) {
+  long double result;
+  if (x != x) {
+    result =  x;
+  } else if (x == S21_INF) {
+    result = S21_INF;
+  } else if (x == -S21_INF) {
+    result = -S21_INF;
+  } else {
+    long long int int_part = (long long int)x;
+    result = (x - int_part != 0 && x < 0) ? int_part - 1 : int_part;
+  }
+  return result;
+}
+
 long double s21_log(double x) {
-  if (x <= 0.0) {
-    return -S21_INFL;
-  }
-  if (x == 1.0) {
-    return 0.0;
-  }
-  if (x < 1.0) {
-    x = 1 / x;
-    return -s21_log(x);
-  }
-  long double result = 0.0;
-  long double term = (x - 1.0) / (x + 1.0);
-  long double term_squared = term * term;
-  long double current_term = term;
-  long double power = 1.0;
-  for (int i = 0; i < 100; i++) {
-    result += current_term / power;
-    current_term *= term_squared;
-    power += 2.0;
-  }
-  return 2 * result;
+    int power = 0;
+    long double result = 0.;
+    long double term = 0;
+
+    if (x != x) {
+        result = x;
+    } else if (x == S21_INF) {
+        result = S21_INF;
+    } else if (x == -S21_INF) {
+        result = -S21_INF;
+    } else if (x <= 0.) {
+        result = -S21_INF;
+    } else if (x != 0) {
+for (; x >= S21_EXP; x /= S21_EXP, power++) continue;
+        for (int i = 0; i < 100; i++) {
+            term = result;
+            result = term + 2 * (x - s21_exp(term)) / (x + s21_exp(term));
+        }
+    } else {
+        result = -S21_INF;
+    }
+    return (result + power);
 }
 
-bool s21_isnan(double n) {
-  d_bits d = {n};
-  return d.bits == NAN_BITS;
-}
 
-bool s21_isinf(double n) {
-  d_bits d = {n};
-  return d.bits == INF_BITS;
-}
