32535c6cf4cc50f14f4736647ed413d34064287b
Dobber Peas <dobberpe@student.21-school.ru>
2024-03-06 22:59:06 +0300

    sin cos still don't pass max double tests

--- src/s21_sin.c
+++ src/s21_sin.c
@@ -1,7 +1,7 @@
 #include "s21_math.h"
 
 long double s21_sin(double x) {
-  x += -(int)(x / (2 * S21_PI)) * (2 * S21_PI);
+  x = s21_fmod(x, S21_PI * 2);
   int n = 0;
   long double sum = 0, sign = 1.0, pow = x, fact = 1.0;
   while (n < 150) {
